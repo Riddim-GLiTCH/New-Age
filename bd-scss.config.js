a19e479446538eebba6e898b7d110b81d977ad01
Riddim-GLiTCH <Riddim_GLITCH@courvix.com>
2023-07-20 09:56:36 +0200

    upload base from create-bd-theme

---
+++ bd-scss.config.js
@@ -0,0 +1,10 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+	meta: {
+		name: "New Age",
+		author: "Riddim-GLiTCH",
+		version: "1.0.0",
+		description: "A futuristic, cyberpunk-style theme with configurable accents and an edgy look.",
+		source: "https://github.com/Riddim-GLiTCH/New Age"
+	}
+}
